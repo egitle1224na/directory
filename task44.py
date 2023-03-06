79571ec32be264ac0605b6737119fed36e641939
Elena <marsik-123@mail.ru>
2023-03-06 17:41:43 +0300

    44

--- task44.py
+++ task44.py
@@ -6,10 +6,8 @@
 # data = pd.DataFrame({'whoAmI':lst})
 # data.head()
 
+
 from sklearn.preprocessing import OneHotEncoder
-
 x = [[0, "human"], [1, "robot"]]
-
 y = OneHotEncoder().fit_transform(x).toarray()
-
 print(y)
